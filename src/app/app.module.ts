a37caded0e10107b1b5d52263f12f6b0f96edec9
Abdelrahman Saeed <AbSaeed@sure.com.sa>
2023-08-22 10:38:16 +0300

    first strucrure ai

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    ContentLayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
